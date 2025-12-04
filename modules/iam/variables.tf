f5f1255495b740c221755727f2fb425ab6cc7244
Kashish Sahu <Kashish_sahu@mckinsey.com>
2025-12-04 14:26:49 +0530

    update eks cersion variable

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -66,7 +66,6 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version to use for the EKS cluster"
   type        = string
-  default     = "1.30"
 }
 
 variable "cluster_endpoint_private_access" {
